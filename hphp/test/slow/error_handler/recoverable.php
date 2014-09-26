4164342d1e2e8866dd8ec0518230d174892f30b3
Reeze Xia <reeze@php.net>
2014-09-27 01:25:45 +0800

    Fix accidentally broken cases

--- hphp/test/slow/error_handler/recoverable.php
+++ hphp/test/slow/error_handler/recoverable.php
@@ -4,9 +4,6 @@ set_error_handler(function($errno, $errmsg) {
 	var_dump($errno == E_RECOVERABLE_ERROR);
 }, E_RECOVERABLE_ERROR);
 
-class X {
-  function test(self $s) {
-    var_dump($s);
-  }
-}
-X::test("hello");
+$closure = function() {};
+
+var_dump($closure->prop);
